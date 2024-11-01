c1d1d2fe19a5042b89518da77aaec234e2077e71
Mark <bagenmar@gmail.com>
2024-11-02 00:08:50 +0200

    papapa

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <raylib.h>
+#include <cmath>
 
 using namespace std;
 
@@ -12,28 +13,32 @@ int main () {
     int ball_speed_x = 5;
     int ball_speed_y = 5;
     int ball_radius = 15;
+    int aim_radius = 30;
+    int aim_x = rand()%(SCREEN_WIDTH-2*aim_radius)+aim_radius;
+    int aim_y = rand()%(SCREEN_HEIGHT-2*aim_radius)+aim_radius;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Prog");
     SetTargetFPS(60);
 
     while (WindowShouldClose() == false){
-   
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
-
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
-        {
-            ball_speed_x *= -1;
-        }
-
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
-        {
-            ball_speed_y *= -1;
-        }
         
+        if(IsKeyDown(KEY_D) && ball_x + ball_radius + ball_speed_x <= SCREEN_WIDTH)
+        ball_x += ball_speed_x;
+        if(IsKeyDown(KEY_A) && ball_x - ball_radius - ball_speed_x >= 0)
+        ball_x -= ball_speed_x;
+        if(IsKeyDown(KEY_W) && ball_y - ball_radius - ball_speed_y >= 0)
+        ball_y -= ball_speed_y;
+        if(IsKeyDown(KEY_S) && ball_y + ball_radius + ball_speed_y <= SCREEN_HEIGHT)
+        ball_y += ball_speed_y;
+        
+        if(sqrt(pow(ball_x-aim_x,2) + pow(ball_y-aim_y,2))<aim_radius){
+            aim_x = rand()%(SCREEN_WIDTH-2*aim_radius)+aim_radius;
+            aim_y = rand()%(SCREEN_HEIGHT-2*aim_radius)+aim_radius;
+        }
         BeginDrawing();
-            ClearBackground(ORANGE);
+            ClearBackground(GRAY);
             DrawCircle(ball_x,ball_y,ball_radius, WHITE);
+            DrawCircle(aim_x,aim_y,aim_radius, RED);
         EndDrawing();
     }
 
