b3571f261005382369fe433cb49c1fbb9a24fb19
aakorkuna <aakorkuna@gmail.com>
2024-11-07 20:31:04 +0200

    papapa

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <raylib.h>
 #include <cmath>
-
+#include "progresbar.cpp"
 #include "properties.cpp"
 #include "button.cpp"
 
@@ -13,6 +13,11 @@ int main () {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME);
     SetTargetFPS(60);
     Button btn(100,100,200,75,40,(char*)"Button");
+    Progressbar pr(200, 200, 200, 75, 40, 25, (char*)"LALA",1);
+    pr.color =  GRAY;
+    pr.beg_color =  LIGHTGRAY;
+    pr.color_bar =  RED;
+    pr.fullbar = 150;
     InitAudioDevice();              
     Music music = LoadMusicStream("resources/musik/trasovuna.mp3");
     PlayMusicStream(music);
@@ -20,10 +25,12 @@ int main () {
     
     while (WindowShouldClose() == false){
         UpdateMusicStream(music); 
+        timePlayed = GetMusicTimePlayed(music)/GetMusicTimeLength(music);
         btn.Calculate();
         BeginDrawing();
         ClearBackground(LIME);
             btn.Draw();
+            pr.Draw();/*
             DrawRectangle(1920-64*5,0,64*5,1080,GRAY);
             DrawRectangle(0,64*15,1920,120,GRAY);
             for(int i = 0; i < 25; i++){
@@ -31,20 +38,14 @@ int main () {
                     if((i+j)%2 == 1) DrawRectangle(i*64,j*64,64,64,ORANGE);
                 }
             }
-            
+            */
+        
         EndDrawing();
-        timePlayed = GetMusicTimePlayed(music)/GetMusicTimeLength(music);
-        if (timePlayed > 1.0f) {
-            timePlayed = 1.0f;
-             BeginDrawing();
-                DrawRectangle(200, 200, (int)(timePlayed*400.0f), 12, MAROON);
-             EndDrawing();
-        }
          
-    return 0;
     }
 
     UnloadMusicStream(music);   // Unload music stream buffers from RAM
     CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
-    CloseWindow();       
+    CloseWindow();    
+    return 0;   
 }
--- progresbar.cpp
+++ progresbar.cpp
@@ -4,30 +4,44 @@
 
 
 using namespace std;
-class progressbar{
+class Progressbar{
     public:
         float pos_x, pos_y;
         int width, height, thickness, fullbar;
         int font_size, len;
         char * text;
-        bool  pressed=false, down = true;
+        bool  pressed=false, down = true, ishorizon;
         Color color, color_bar, beg_color, text_color = BLACK;
         Font font = LoadFontEx("resources/fonts/Panoptica.ttf", 40, 0, 0);
 
 
         void Draw(){
-            DrawRectangle(pos_x, pos_y, width, height, color);
-            DrawRectangle(pos_x+thickness, pos_y+thickness, fullbar ,height-2*thickness,color_bar);
-            DrawRectangleLines(pos_x+thickness, pos_y+thickness, width-2*thickness, height-2*thickness, beg_color);
-            int t_w = MeasureText(text,font_size);
-            DrawTextEx(font,text,(Vector2){ pos_x+(width-t_w)/2, pos_y+(height-font.baseSize)/2},font_size,0,text_color);
+            
+
+            if (ishorizon == true){
+                DrawRectangle(pos_x, pos_y, width, height, color);
+                DrawRectangle(pos_x+thickness, pos_y+thickness, width-2*thickness, height-2*thickness, beg_color);
+                DrawRectangle(pos_x+thickness, pos_y+thickness, ((width-2*thickness)/100)*fullbar ,height-2*thickness,color_bar);
+                int t_w = MeasureText(text,font_size);
+                DrawTextEx(font,text,(Vector2){ pos_x+(width-t_w)/2, pos_y+(height-font.baseSize)/2},font_size,0,text_color);
+            }
+            else {
+                DrawRectangle(pos_x, pos_y, height, width, color);
+                DrawRectangle(pos_x+thickness, pos_y+thickness, height-2*thickness,width-2*thickness , beg_color);
+                DrawRectangle(pos_x+thickness, pos_y+thickness, height-2*thickness ,((width-2*thickness)/100)*fullbar,color_bar);
+                int t_w = MeasureText(text,font_size);
+                DrawTextEx(font,text,(Vector2){ pos_x+(width-t_w)/2, pos_y+(height-font.baseSize)/2},font_size,0,text_color);
+            }
+            
         }
-        progressbar(float x, float y, int w, int h, int f_s, char t[]){
+        Progressbar(float x, float y, int w, int h, int f_s, int th, char t[],  bool hor){
             pos_x = x;
             pos_y = y;
             width = w;
             height = h;
             text = t;
             font_size = f_s;
+            ishorizon = hor;
+            thickness = th;
         }
 };
